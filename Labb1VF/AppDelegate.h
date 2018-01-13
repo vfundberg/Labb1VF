ea7b4f49b69eaede7187707c73af5993fd3ba569
Fundberg <vfundberg@gmail.com>
2018-01-13 11:52:12 +0100

    First commit

---
+++ Labb1VF/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Labb1VF
+//
+//  Created by Victor Fundberg on 2018-01-13.
+//  Copyright © 2018 Victor Fundberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Labb1VF/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Labb1VF
+//
+//  Created by Victor Fundberg on 2018-01-13.
+//  Copyright © 2018 Victor Fundberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
