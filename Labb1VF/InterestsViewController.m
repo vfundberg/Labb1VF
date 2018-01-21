e2ef1d693d4ed7983241ac80b5286d91e233ec02
Fundberg <vfundberg@gmail.com>
2018-01-21 21:35:38 +0100

    Färdig app

---
+++ Labb1VF/InterestsViewController.m
@@ -0,0 +1,37 @@
+//
+//  InterestsViewController.m
+//  Labb1VF
+//
+//  Created by Victor Fundberg on 2018-01-21.
+//  Copyright © 2018 Victor Fundberg. All rights reserved.
+//
+
+#import "InterestsViewController.h"
+
+@interface InterestsViewController ()
+
+@end
+
+@implementation InterestsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
